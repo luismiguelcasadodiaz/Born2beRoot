434eb0016289e474618ab6152766666c308a1d73
Luis Miguel Casado Díaz <luismiguelcasadodiaz@gmail.com>
2023-11-15 00:13:36 +0100

    CPU USAGE PERCENTAJE

--- monitoring.sh
+++ monitoring.sh
@@ -16,6 +16,8 @@ AVAI_RAM=`cat  /proc/meminfo |grep MemAvailable | sed 's/MemAvailable://' | sed 
 USED_RAM=`bc <<< "scale=2; (${TOTAL_RAM} - ${AVAI_RAM}) / 1024 / 1024"`
 TOTAL_RAM=`bc <<< "scale=2; ${TOTAL_RAM} / 1024 / 1024"`
 USED_RAM_PERC=`bc <<< "scale=2; (${USED_RAM} / ${TOTAL_RAM}) * 100"`
+CPU_USAGE_RATE=`cat /proc/stat | grep 'cpu ' | sed 's/cpu  //g' | awk  '{split($0,t," "); for(i=NF;i>0;i--) s = s + $i } END {print 1 - ($4/s) }'`
+CPU_USAGE_PERC=`bc <<< "scale=2; ($CPU_USAGE_RATE * 100)"`
 LAST_BOOT=`who -b | sed 's/[a-z ]*//'`
 LVM_IN_USE="NO"
 if  [[ $(grep 'mapper' /etc/fstab | wc -l) -gt 0 ]];
@@ -38,6 +40,7 @@ echo -e "${WHITE}Physical cores         :${GREEN}$PHYSICAL_CORES"
 echo -e "${WHITE}virtual cores          :${GREEN}$VIRTUAL_CORES"
 echo -e "${WHITE}Total memory           :${GREEN}$TOTAL_RAM GB"
 echo -e "${WHITE}Used memory            :${GREEN}$USED_RAM GB (${USED_RAM_PERC}%)"
+echo -e "${WHITE}CPU usage              :${GREEN}$CPU_USAGE_PERC %"
 echo -e "${WHITE}Last boot time         :${GREEN}$LAST_BOOT"
 echo -e "${WHITE}LVM in use             :${GREEN}$LVM_IN_USE"
 echo -e "${WHITE}TCP active connections :${GREEN}$ACTIVE_CONNECTIONS"
