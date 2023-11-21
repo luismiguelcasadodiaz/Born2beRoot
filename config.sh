cb7799b5b179d26a0e4bbfe033322e37703b7772
root <root@luicasad42.42barcelona.com>
2023-11-21 15:36:38 +0100

    a little advance wiht password

--- config.sh
+++ config.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/bash
+
+# Back up of ssh server original configuration file.
+
 cp /etc/ssh/sshd_config /etc/ssh/sshd_config.$EPOCHSECONDS.bck
 
 sed -i -e '/#Port 22/ s/#Port 22/Port 4242/' /etc/ssh/sshd_config
@@ -7,5 +10,20 @@ sed -i -e '/^PermitRootLogin no/a AllowUsers luicasad' /etc/ssh/sshd_config
 sed -i -e '/^#PasswordAuthentication/a ChallengeResponseAutenthication yes' /etc/ssh/sshd_config
 sed -i -e '/#Banner none/ s/#Banner none/Banner \/etc\/ssh/global_banner.txt' /etc/ssh/sshd_config
 
+
+
+#set up strong password policy.
+cp /etc/login.defs /etc/login.defs$EPOCHSECONDS.bck
+
+sed -i -e '/PASS_MAX_DAYS/ s/99999/30/' /etc/login.defs
+sed -i -e '/PASS_MIN_DAYS/ s/0/2/' /etc/login.defs
+sed -i -e '/PASS_WARN_AGE/ s/7/7/' /etc/login.defs
+cp /etc/pam.d/common-password /etc/pam.d/common-password.$EPOCHSECONDS.bck
+
+
+
+:q
+
+# set up firewall rules.
 ufw deny 22
 ufw allow 4242
--- install.sh
+++ install.sh
@@ -1,4 +1,26 @@
 #!/usr/bin/bash
+apt install -y bc
 apt install -y openssh-server
 apt install -y ufw
 apt install -y sudo
+apt install -y libpam-pwquality
+apt install -y libpam-google-authenticator 
+apt install -y wget
+apt install -y nzip
+apt install -y lighttpd
+apt install -y php
+apt install -y php-curl
+apt install -y php-fpm
+apt install -y php-bcmath
+apt install -y php-gd
+apt install -y php-soap
+apt install -y php-zip
+apt install -y php-mbstring
+apt install -y php-mysqlnd
+apt install -y php-xml
+apt install -y php-intl
+apt install -y mariadb-server
+apt install -y mariadb-client
+wget https://wordpress.org/latest.zip
+apt install -y vsftpd
+
