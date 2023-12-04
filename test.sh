70dc89699d9455bd8441ea0be1550d64408d50e4
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2023-12-04 10:22:54 +0100

    script review

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/bash
 
+
 # Operating system
 KERNEL_RELEASE=`uname --kernel-release`
 KERNEL_VERSION=`uname --kernel-version`
@@ -14,27 +15,17 @@ VIRTUAL_CORES=`grep "^processor" /proc/cpuinfo | sort | uniq | wc -l `
 # Memory usage
 TOTAL_RAM=`cat  /proc/meminfo | grep MemTotal | sed 's/MemTotal://' | sed 's/ //g' | sed 's/kB//'`
 AVAI_RAM=`cat  /proc/meminfo | grep MemAvailable | sed 's/MemAvailable://' | sed 's/ //g' | sed 's/kB//'`
-#USED_RAM=`bc <<< "scale=2; (${TOTAL_RAM} - ${AVAI_RAM}) / 1024 / 1024"`
-#TOTAL_RAM=`bc <<< "scale=2; ${TOTAL_RAM} / 1024 / 1024"`
-#USED_RAM_PERC=`bc <<< "scale=2; (${USED_RAM} / ${TOTAL_RAM}) * 100"`
-#printf -v USED_RAM "%4.2f" $USED_RAM 
-
-# CPU usage 
-CPU_USAGE_RATE=`cat /proc/stat | grep 'cpu ' | sed 's/cpu  //g' | awk  '{split($0,t," "); for(i=NF;i>0;i--) s = s + $i } END {print 1 - ($4/s) }' | sed 's/\,/\./'`
-#CPU_USAGE_PERC=`bc <<< "scale=2; 100 * ${CPU_USAGE_RATE}"`
-#CPU_USAGE_PERC=`echo $CPU_USAGE_PERC | sed 's/\./\,/'`
-#printf -v CPU_USAGE_PERC "%4.2f" $CPU_USAGE_PERC 
 
 # Disk usage
-DISK_TOT=`df -m | grep "/dev/" | awk '{disks_size += $2} END {print disks_size}'`
+DISK_TOT=`df -m | grep "/dev/" | awk '{disks_size += $2} END {printf (), disks_size/1024}'`
 DISK_USE=`df -m | grep "/dev/" | awk '{disks_size += $3} END {print disks_size}'`
-#DISK_PER=`bc <<< "scale=2; 100 * $DISK_USE / $DISK_TOT"`
 
 # Boot time
 LAST_BOOT=`who -b | sed 's/[a-z ]*//'`
 
 # Logic volumen manager
 LVM_IN_USE=$(if [ $(lsblk | grep 'lvm' | wc -l) -gt 0 ]; then echo YES; else echo NO; fi)
+
 
 # Connections
 ACTIVE_CONNECTIONS=`ss -Hlt | wc -l`
@@ -50,8 +41,7 @@ MAC_ADDRESS=`ip link | grep ether | awk '{print $2}'`
 SUDO_COMMANDS=`journalctl  /usr/bin/sudo | grep COMMAND | wc -l`
 
 
-wall  "
------------------------------------------
+wall "Aprendiendo cron
 Arquitecture           :$MACHINE_ARCHITECTURE
 Operating system       :$OPERATING_SYSTEM
 Kernel Release         :$KERNEL_RELEASE
@@ -69,7 +59,6 @@ LVM in use             :$LVM_IN_USE
 TCP active connections :$ACTIVE_CONNECTIONS
 Looged users           :$LOGGED_USERS
 IP(v4) address         :$IP_ADDRESS ($MAC_ADDRESS)
-Sudo commands executed :$SUDO_COMMANDS
------------------------------------------
+Sudo commands executed :$SUDO_COMMANDS"
 "
 
